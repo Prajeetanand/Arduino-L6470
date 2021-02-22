5e57350d0552bc3e4b83a881d983ea4b4f610742
Prajeetanand <prajeet.anand@gmail.com>
2021-02-22 10:47:14 -0500

    Update single_motor.ino
    no change

--- examples/single_motor/single_motor.ino
+++ examples/single_motor/single_motor.ino
@@ -1,11 +1,11 @@
 #include <L6470.h>
 
 #define SS_PIN     10
-#define SCK_PIN    11
-#define MOSI_PIN   12
-#define MISO_PIN   13
-#define RESET_PIN  14
-#define BUSYN_PIN  15
+#define SCK_PIN    13
+#define MOSI_PIN   11
+#define MISO_PIN   12
+#define RESET_PIN  8
+#define BUSYN_PIN  9
 
 L6470 stepper(SS_PIN);              // create stepper object
 
@@ -15,7 +15,7 @@ void setup() {
   stepper.setAcc(100);              // Set acceleration
   stepper.setMaxSpeed(800);
   stepper.setMinSpeed(1);
-  stepper.setMicroSteps(2);         // 1,2,4,8,16,32,64 or 128
+  stepper.setMicroSteps(128);         // 1,2,4,8,16,32,64 or 128
   stepper.setThresholdSpeed(1000);
   stepper.setOverCurrent(6000);     // Set overcurrent protection
   stepper.setStallCurrent(3000);
